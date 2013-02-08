ead644cb7f31b041f4e5e6d345df46c6dfa772f0
Sam Brotherton <sbrother@gmail.com>
2013-02-08 01:33:12 -0800

    added basic graph

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-# from kivy.uix.listview import ListView
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy_plotter.plot import Plot 
+from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
 
 Builder.load_file('ui.kv')
 
 class MainView(Widget):
-    pass
+    visualizer = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(MainView, self).__init__(**kwargs)
+        
+        self.setup_visualizer()
+
+
+    def setup_visualizer(self):
+        v = self.visualizer
+        v.border_width = 2
+        v.border_color = (.7, .7, .7)
+        v.tick_color = (.7, .7, .7)
+        v.y_axis_title = "DA Transient Amplitude (nM)"
+        v.x_axis_title = "Time (sec)"
+        v.viewport = [0,0,50,100]
+        v.tick_distance_x = 10
+        v.tick_distance_y = 20
+
+
 
 class ListBox(Widget):
     pass
